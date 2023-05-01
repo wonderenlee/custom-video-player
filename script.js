ed8109e16593df63484b8afc1c04cb4eab561e5b
wonderenlee <wonderenlee@gmail.com>
2023-05-01 11:08:40 -0400

    initial custom-video-player

---
+++ script.js
@@ -0,0 +1,51 @@
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// play & pause video
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+// update play/pause icon
+function updatePlayIcon() {
+    if (video.paused) {
+    play.innerHTML = '<i class = "fa fa-play fa-2x"></i';
+    } else {
+    play.innerHTML = '<i class = "fa fa-pause fa-2x"></i';
+    }
+}
+
+// update progress & timestamp
+function updateProgress() {
+    progress.value = (video.currentTime / video.duration) * 100;
+}
+
+// set video time to progress
+function setVideoProgress() {
+    return true;
+}
+
+// stop video
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
+
+// event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
